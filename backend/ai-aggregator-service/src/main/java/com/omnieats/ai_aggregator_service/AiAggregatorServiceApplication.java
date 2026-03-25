6764275b7c3e920c8c2853d12a7decb7dc8d2c4d
dveduard <dveduard@proton.me>
2026-03-25 07:28:58 +0200

    build: scaffold base parent pom and 4 core microservices

---
+++ backend/ai-aggregator-service/src/main/java/com/omnieats/ai_aggregator_service/AiAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.omnieats.ai_aggregator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAggregatorServiceApplication.class, args);
+	}
+
+}
